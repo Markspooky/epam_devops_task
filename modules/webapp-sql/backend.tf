385b7751fc73af94f640a4403ad177172fd215f7
Markspooky <80107471+Markspooky@users.noreply.github.com>
2025-11-09 14:30:53 +0100

    Add files via upload

---
+++ modules/webapp-sql/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.42.0"
+    }
+  }
+  required_version = ">= 1.3.8"
+  # backend "azurerm" {
+  # }
+}
